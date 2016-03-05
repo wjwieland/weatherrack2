d75a388edebc3a4ae615fc654e1ab96b5e2e8d7a
wjwieland <wjw@wjwieland.com>
2016-03-05 08:34:35 -0600

    Updted for new light sensor

--- weatherrack2.ino
+++ weatherrack2.ino
@@ -24,16 +24,20 @@
  Attiny 25/45/85: All pins are usable
  ATmega644P/ATmega1284P: All pins are usable
  */
+#include <Wire.h>
 #include <PinChangeInterrupt.h>
 #include <ArduinoJson.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
 #include <TimerObject.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+
 
 // choose a valid PinChangeInterrupt pin of your Arduino board
 #define pinVel 9
 #define pinRain 10
-#define pinTmp 5
+#define pinTmp 6
 #define ONE_WIRE_BUS pinTmp
 #define pinLux A3
 #define pinDir A0
@@ -41,15 +45,18 @@
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress thermometer = { 0x28, 0xC1, 0xD1, 0xDC, 0x06, 0x00, 0x00, 0xE7 };
+Adafruit_TSL2561_Unified tsl0 = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
 volatile unsigned long int vel = 0;
 volatile unsigned long int rain_cnt = 0;
 const float per_tip = 0.0110;
-int rpt_ms = 15000;
+int rpt_ms = 5000;
 int rpt_sec = rpt_ms / 1000;
 int analog_lux = 0;
 int battVolts;
 boolean debugger = false;
+uint16_t broadband = 0;
+uint16_t infrared = 0;
 
 //3.3V
 float volts[8] = {
@@ -78,7 +85,7 @@ void velocity(void);
 void rain(void);
 void dbug();
 int getBandgap(void);
-int getLux(void);
+float get_lux0(void);
 void make_json();
 TimerObject *report = new TimerObject(rpt_ms);
 TimerObject *debug  = new TimerObject(rpt_ms);
@@ -91,7 +98,16 @@ void setup() {
 
   attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(pinVel), velocity, RISING);
   attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(pinRain), rain, RISING);
-
+//  get_i2c(); This does not seem to work on pro trinket.  works fine on pro mini
+  if(!tsl0.begin()) {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.print("Ooops, first TSL2561 not detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  } else {
+    configureSensor();
+    displaySensorDetails();
+  }
+  
   sensors.begin();
   // set the resolution to 10 bit (good enough?)
   sensors.setResolution(thermometer, 10);
@@ -126,7 +142,7 @@ void dbug() {
   Serial.print("Oper. Voltage ");
   Serial.println(getBandgap());
   Serial.print("light reading ");
-  Serial.println(getLux());
+  Serial.println(get_lux0());
   Serial.println();
   Serial.println("Debug End");
 }
--- wr2_functions.ino
+++ wr2_functions.ino
@@ -10,12 +10,6 @@ int get_dir() {
   }
 }
 //####################################################
-//returns analog value from light sensor
-int getLux() {
-  return analogRead(pinLux);
-}
-
-//####################################################
 //returns inches
 float get_rain() {
   return (rain_cnt * per_tip) / 2 ;
@@ -30,7 +24,9 @@ void make_json() {
   root["rr"] = rpt_rain_rate();
   root["tF"] = rpt_temp();
   root["ov"] = getBandgap() / 100.00;
-  root["lux"] = getLux() / 100.00;
+  root["lux"] = get_lux0();
+  root["bbl"] = get_bb_light();
+  root["irl"] = get_ir_light();
   root.printTo(Serial);
   Serial.println();
   rain_cnt = 0;
@@ -103,4 +99,98 @@ int getBandgap(void)  {    // Returns actual value of Vcc (x 100) {
 }
 
 /**************************************************************************/
+void configureSensor(void)
+{
+  /* You can also manually set the gain or enable auto-gain support */
+  // tsl0.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
+  // tsl0.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
+  tsl0.enableAutoRange(true); 
+  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
+  //tsl0.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  //tsl0.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+  tsl0.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+  /* Update these values depending on what you've set above! */  
+  Serial.println("------------------------------------");
+  Serial.print  ("Gain:         "); Serial.println("Auto");
+  Serial.print  ("Timing:       "); Serial.println("402 ms");
+  Serial.println("------------------------------------");
+}
+//#################################################################################
+float get_lux0() {
+   sensors_event_t event;
+   tsl0.getEvent(&event);
+  /* Display the results (light is measured in lux) */
+  if (event.light)
+  {
+//    Serial.print(event.light); Serial.print(" lux S1   ");
+    return event.light;
+  }
+  else
+  {
+    /* If event.light = 0 lux the sensor is probably saturated
+       and no reliable data could be generated! */
+    Serial.println("Sensor overload");
+  }
+}
+//#####################################################################
+float get_bb_light() { 
+  tsl0.getLuminosity(&broadband, &infrared); //return broadband light level
+  return broadband;
+}
+//#####################################################################
+float get_ir_light() { //return infra-red light level
+  tsl0.getLuminosity(&broadband, &infrared);
+  return infrared;
+}
 
+//#####################################################################
+
+void get_i2c() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    Serial.print("Address "); Serial.println(address);
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+}
+//############################################################################3
+void displaySensorDetails(void) {
+  sensor_t sensor;
+  tsl0.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" lux");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" lux");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+}
